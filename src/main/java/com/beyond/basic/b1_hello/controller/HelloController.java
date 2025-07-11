276e9c27700e2c991276a4d41bb21ad171e126f6
Jihyeon0804 <cam080421@naver.com>
2025-07-11 11:52:19 +0900

    Hello Controller

--- src/main/java/com/beyond/basic/b1_hello/controller/HelloController.java
+++ src/main/java/com/beyond/basic/b1_hello/controller/HelloController.java
@@ -3,12 +3,14 @@ package com.beyond.basic.b1_hello.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-// Component 어노테이션을 통해 별도의 객체를 생성할 필요가 없는, 싱글톤 객체 생성
+// Component 어노테이션을 통해 별도의 객체를 생성할 필요가 없는 싱글톤 객체 생성
 // Controller 어노테이션을 통해 쉽게 사용자의 http request를 분석하고 http response를 생성
 @Controller
-// 클래스 차원 url 매핑시에는 RequestMapping을 사용
+// 클래스 차원 url(공통된 url 주소) 매핑시에는 RequestMapping을 사용
 @RequestMapping("/hello")
 public class HelloController {
 
@@ -48,7 +50,8 @@ public class HelloController {
     // /hello/param2?name=hong&email=hong@naver.com
     @GetMapping("/param2")
     @ResponseBody
-    public String param2(@RequestParam(value = "name") String inputName, @RequestParam(value = "email") String inputEmail) {
+    public String param2(@RequestParam(value = "name") String inputName,
+                         @RequestParam(value = "email") String inputEmail) {
         System.out.println(inputName);
         System.out.println(inputEmail);
         return "ok";
@@ -60,10 +63,88 @@ public class HelloController {
     // restful : api로서 구조가 합의한대로 잘 짜여진 형식
     @GetMapping("/path/{inputId}")
     @ResponseBody
-    public String path(@PathVariable long inputId) {
+    public String path(@PathVariable Long inputId) {
         // 별도의 형 변환 없이도 매개변수에 타입 지정 시 자동 형변환 시켜줌
 //        long id = Long.parseLong(inputId);
         System.out.println(inputId);
         return "OK";
     }
+    
+    // case6. parameter가 많아질 경우, 데이터 바인딩을 통해 input값 처리
+    // 데이터 바인딩 : parameter를 사용하여 객체로 생성
+    // ?name=hong&email=hong@naver.com
+    @GetMapping("/param3")
+    @ResponseBody
+//    public String param3(Hello hello) {
+    // @ModelAttribute를 써도 되고 안써도 되는데, 이 키워드를 써서 명시적으로 param의 형식의 데이터를 받겠다라는 것을 표현
+    public String param3(@ModelAttribute Hello hello) {
+        // 수 많은 파라미터가 넘어오면 어차피 객체로 만들 것이기 때문에
+        // 애초에 받아올 때부터 객체로 받아오기
+        return "OK";
+    }
+
+    // case7. 서버에서 화면을 return, 사용자로부터 넘어오는 input 값을 활용하여 동적인 화면 생성
+    // 서버에서 화면(+데이터)을 렌더링 해주는 SSR 방식 (CSR 방식은 서버에서 데이터만)
+    // MVC(Model, View, Controller) 패턴이라고도 함
+    @GetMapping("/model-param")
+    public String modelParm(@RequestParam(value = "id") Long inputId, Model model) {
+        // Model 객체는 데이터를 화면에 전달해주는 역할
+        // name 이라는 key에 hong 이라는 value를 key : value 형태로 화면에 전달
+        if (inputId == 1) {
+            model.addAttribute("name", "hong");
+            model.addAttribute("email", "hong@naver.com");
+        } else if (inputId == 2) {
+            model.addAttribute("name", "hong2");
+            model.addAttribute("email", "hong2@naver.com");
+        }
+        return "helloworld2";
+    }
+
+
+    // post 요청의 case들 : url 인코딩 방식과 multipart-formdata, json
+    // case1. text만 있는 form-data  형식
+    // 형식 : body에 name=xxx&email=xxx
+    @GetMapping("/form-view")
+    public String formView() {
+        return "form-view";
+    }
+
+    @PostMapping("/form-view")
+    @ResponseBody
+    // get 요청에 url에 파라미터 방식과 동일한 데이터 형식이므로 RequestParam 또는 데이터 바인딩 방식 가능
+    public String formViewPost(@ModelAttribute Hello hello) {
+        System.out.println(hello);
+        return "OK";
+    }
+
+
+    // case2-1. text와 file이 있는 form-data 형식 (순수 html로 제출)
+    @GetMapping("/form-file-view")
+    public String formFileView() {
+        return "form-file-view";
+    }
+
+    @PostMapping("/form-file-view")
+    @ResponseBody
+    public String formFileViewPost(@ModelAttribute Hello hello,
+                                   @RequestParam(value = "photo") MultipartFile photo) {
+        System.out.println(hello);
+        System.out.println(photo.getOriginalFilename());
+        return "OK";
+    }
+
+    // case2-2. text와 file이 있는 form-data 형식 (js로 제출)
+
+
+    // case3. text와 멀티 file이 있는 form-data 형식
+
+
+    // case4. json 데이터 전송
+
+    
+    // case5. 중첩된 json 데이터 처리
+
+
+    // case6. json과 file을 같이 처리할 때
+
 }
