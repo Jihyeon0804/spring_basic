8369652415f5146494c86b911162bed75e4c6c62
Jihyeon0804 <cam080421@naver.com>
2025-07-14 16:12:35 +0900

    Author Detail DTO

---
+++ src/main/java/com/beyond/basic/b2_board/dto/AuthorDetailDTO.java
@@ -0,0 +1,16 @@
+package com.beyond.basic.b2_board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorDetailDTO {
+
+    private Long id;
+    private String name;
+    private String password;
+
+}
