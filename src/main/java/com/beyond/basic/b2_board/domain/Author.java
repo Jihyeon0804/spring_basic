921cfb7c88f17de1349901966ae466216453e9dc
Jihyeon0804 <cam080421@naver.com>
2025-07-15 19:16:59 +0900

    Author

--- src/main/java/com/beyond/basic/b2_board/domain/Author.java
+++ src/main/java/com/beyond/basic/b2_board/domain/Author.java
@@ -1,13 +1,20 @@
 package com.beyond.basic.b2_board.domain;
 
+import com.beyond.basic.b2_board.dto.AuthorDetailDTO;
+import com.beyond.basic.b2_board.dto.AuthorListDTO;
 import com.beyond.basic.b2_board.repository.AuthorMemoryRepository;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Author {
 
     private Long id;
@@ -16,7 +23,7 @@ public class Author {
     private String password;
 
     public Author(String name, String email, String password) {
-        this.id = AuthorMemoryRepository.id;
+//        this.id = AuthorMemoryRepository.id;          // db 연결 시 필요X
         this.name = name;
         this.email = email;
         this.password = password;
@@ -24,5 +31,13 @@ public class Author {
 
     public void updatePw(String password) {
         this.password = password;
+    }
+
+    public AuthorDetailDTO detailFromEntity() {
+        return new AuthorDetailDTO(this.id, this.name, this.email);
+    }
+
+    public AuthorListDTO listFromEntity() {
+        return new AuthorListDTO(this.id, this.name, this.email);
     }
 }
