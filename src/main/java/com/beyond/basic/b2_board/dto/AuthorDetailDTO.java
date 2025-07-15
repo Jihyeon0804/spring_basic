76e633e184c1204e928cec2253d3eaa7b39ef423
Jihyeon0804 <cam080421@naver.com>
2025-07-15 19:17:37 +0900

    Author Detail DTO

--- src/main/java/com/beyond/basic/b2_board/dto/AuthorDetailDTO.java
+++ src/main/java/com/beyond/basic/b2_board/dto/AuthorDetailDTO.java
@@ -1,5 +1,6 @@
 package com.beyond.basic.b2_board.dto;
 
+import com.beyond.basic.b2_board.domain.Author;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,5 +13,10 @@ public class AuthorDetailDTO {
     private Long id;
     private String name;
     private String email;
+    
+    // 한 개의 Entity로만 DTO가 조립되는 것이 아니기 때문에 DTO 계층에서 fromEntity 설계
+    public static AuthorDetailDTO fromEntity(Author author) {
+        return new AuthorDetailDTO(author.getId(), author.getName(), author.getEmail());
+    }
 
 }
