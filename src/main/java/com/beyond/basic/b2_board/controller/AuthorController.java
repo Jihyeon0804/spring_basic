45c43edb9920473afdf0ac0729f1a4bec40ba75a
Jihyeon0804 <cam080421@naver.com>
2025-07-15 19:17:16 +0900

    Author Controller

--- src/main/java/com/beyond/basic/b2_board/controller/AuthorController.java
+++ src/main/java/com/beyond/basic/b2_board/controller/AuthorController.java
@@ -1,19 +1,19 @@
 package com.beyond.basic.b2_board.controller;
 
 import com.beyond.basic.b2_board.domain.Author;
-import com.beyond.basic.b2_board.dto.AuthorCreateDTO;
-import com.beyond.basic.b2_board.dto.AuthorDetailDTO;
-import com.beyond.basic.b2_board.dto.AuthorListDTO;
-import com.beyond.basic.b2_board.dto.AuthorUpdatePwDTO;
+import com.beyond.basic.b2_board.dto.*;
 import com.beyond.basic.b2_board.service.AuthorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @RestController             // @Controller + @ResponseBody : 데이터만 주고 받음, 화면X
-@RequiredArgsConstructor
+@RequiredArgsConstructor    // 생성자 주입 (반드시 final 붙여주어야 함)
 @RequestMapping("/author")
 public class AuthorController {
 
@@ -21,14 +21,25 @@ public class AuthorController {
 
     // 회원 가입
     @PostMapping("/create")
-    public String save(@RequestBody AuthorCreateDTO authorCreateDTO) {
-        try {
+//    public ResponseEntity<String> save(@RequestBody AuthorCreateDTO authorCreateDTO) {          // 입력값과 도메인 필드가 다르면 새로운 객체 생성(사용자 입력과 응답 DTO 따로 설계; DTO 에는 setter 설정 포함(@Data), 일반 도메인에는 getter만(@DataX))
+    public ResponseEntity<?> save(@RequestBody AuthorCreateDTO authorCreateDTO) {
+//    public String save(@RequestBody AuthorCreateDTO authorCreateDTO) {
+        /*
+        try {       // error 발생했는데  try-catch 해주지 않으면 500 Internal Server Error
             this.authorService.save(authorCreateDTO);
+//            return new ResponseEntity<>("OK", HttpStatus.CREATED);
+            return new ResponseEntity<>(new CommonDTO("OK", HttpStatus.OK.value(), HttpStatus.OK.getReasonPhrase()), HttpStatus.CREATED);
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
-            return e.getMessage();
+            // 생성자 매개변수로 응답을 줄 body 부분의 객체와 header 부의 상태코드 입력
+//            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new CommonDTO("BAD REQUEST", HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase()), HttpStatus.BAD_REQUEST);
+//            return e.getMessage();          // return String -> 200 OK
         }
-        return "OK";
+        */
+        // controllerAdvice가 없었으면 위와 같이 개별적인 예외 처리가 필요하나, 이제는 전역적인 예외 처리가 가능
+        this.authorService.save(authorCreateDTO);
+        return new ResponseEntity<>("OK", HttpStatus.CREATED);
     }
     
     // 회원 목록 조회 : /author/list
@@ -37,18 +48,25 @@ public class AuthorController {
         return authorService.findAll();
     }
     
-    
+
     // 회원 상세 조회 by id : /author/detail/1
     // 서버에서 별도의 try-catch를 하지 않으면, 에러 발생 시 500 error + 스프링의 포맷으로 error return
     @GetMapping("/detail/{id}")
 //    public Author findById(@PathVariable Long id) {
-    public AuthorDetailDTO findById(@PathVariable Long id) {
+//    public AuthorDetailDTO findById(@PathVariable Long id) {
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        /*
         try {
-            return authorService.findById(id);
-        } catch (IllegalArgumentException e) {
+//            return new ResponseEntity<>(authorService.findById(id), HttpStatus.OK);
+            return new ResponseEntity<>(new CommonDTO(authorService.findById(id), HttpStatus.OK.value(), "author is found"), HttpStatus.OK);
+//            return authorService.findById(id);
+        } catch (NoSuchElementException e) {
             e.printStackTrace();
+            return new ResponseEntity<>(new CommonErrorDTO(HttpStatus.NOT_FOUND.value(), "author is not found"), HttpStatus.NOT_FOUND);
+//            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
-        return null;
+         */
+        return new ResponseEntity<>(new CommonDTO(authorService.findById(id), HttpStatus.OK.value(), "author is found"), HttpStatus.OK);
     }
     
     
