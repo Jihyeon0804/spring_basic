5a6104bd84b7fc4f1dbf057ab0b881e27cda199b
Jihyeon0804 <cam080421@naver.com>
2025-07-16 15:22:54 +0900

    Author Detail DTO

--- src/main/java/com/beyond/basic/b2_board/dto/AuthorDetailDTO.java
+++ src/main/java/com/beyond/basic/b2_board/dto/AuthorDetailDTO.java
@@ -15,6 +15,7 @@ public class AuthorDetailDTO {
     private String email;
     
     // 한 개의 Entity로만 DTO가 조립되는 것이 아니기 때문에 DTO 계층에서 fromEntity 설계
+    // 그 외 필요한 데이터가 있다면 파라미터로 여러 개 받아서 조립해서 return
     public static AuthorDetailDTO fromEntity(Author author) {
         return new AuthorDetailDTO(author.getId(), author.getName(), author.getEmail());
     }
