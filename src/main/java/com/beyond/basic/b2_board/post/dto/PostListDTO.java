27481f09da01544aade3a927c34b9a3f65124011
Jihyeon0804 <cam080421@naver.com>
2025-07-18 15:20:19 +0900

    Post List DTO

--- src/main/java/com/beyond/basic/b2_board/post/dto/PostListDTO.java
+++ src/main/java/com/beyond/basic/b2_board/post/dto/PostListDTO.java
@@ -15,11 +15,13 @@ public class PostListDTO {
 
     private Long id;
     private String title;
+    private String authorEmail;
 
     public static PostListDTO fromEntity(Post post) {
         return PostListDTO.builder()
                 .id(post.getId())
                 .title(post.getTitle())
+                .authorEmail(post.getAuthor().getEmail())
                 .build();
     }
 
