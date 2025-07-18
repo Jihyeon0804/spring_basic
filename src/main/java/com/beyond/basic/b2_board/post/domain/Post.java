23ce6e231d0ca1978f5ecdaedcf1f2dc19b2e442
Jihyeon0804 <cam080421@naver.com>
2025-07-18 15:19:50 +0900

    Post

--- src/main/java/com/beyond/basic/b2_board/post/domain/Post.java
+++ src/main/java/com/beyond/basic/b2_board/post/domain/Post.java
@@ -23,6 +23,9 @@ public class Post extends BaseTimeEntity {
     @Column(length = 3000)
     private String contents;
 
+    // 게시글 삭제 여부 (게시글 목록 조회 시 "Y"는 제외)
+    private String delYn;
+
     // FK 설정 시 ManyToOne 필수
     // ManyToOne 에서는 default FetchType.EAGER (즉시 로딩) : 참조 테이블 바로 조회; author 객체를 사용하지 않아도 author 테이블로 쿼리 발생
     // 그래서 일반적으로 FetchType.LAZY (지연 로딩) 설정 : 테이블 참조 시 조회; author 객체를 사용하지 않는 한, author 테이블 쿼리 발생X
