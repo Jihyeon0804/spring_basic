4d48c8cfe53ae6e0f9776e07868f9d7db23ed4d8
Jihyeon0804 <cam080421@naver.com>
2025-07-18 10:15:38 +0900

    Author

--- src/main/java/com/beyond/basic/b2_board/author/domain/Author.java
+++ src/main/java/com/beyond/basic/b2_board/author/domain/Author.java
@@ -1,16 +1,12 @@
 package com.beyond.basic.b2_board.author.domain;
 
-import com.beyond.basic.b2_board.author.dto.AuthorDetailDTO;
 import com.beyond.basic.b2_board.author.dto.AuthorListDTO;
+import com.beyond.basic.b2_board.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import java.time.LocalDateTime;
 
 // JPA를 사용할 경우 @Entity를 반드시 붙어야하는 어노테이션 (매핑할 기준이 되는 Entity 설정을 위해 사용)
 // JPA의 EntityManager 에게 객체를 위임하기 위한 어노테이션
@@ -20,7 +16,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class Author {
+public class Author extends BaseTimeEntity {
 
     // 해당 컬럼 PK 설정
     @Id
@@ -33,27 +29,27 @@ public class Author {
     private String email;
 //    @Column(name = "pw")  :   되도록이면 컬럼명과 변수명을 일치시키는 것이 개발의 혼선을 줄일 수 있음.
     private String password;
-    // 컬럼명에 캐멀 케이스 사용 시, db에는 created_time으로 컬럼 생성
-    @CreationTimestamp
-    private LocalDateTime createdTime;
-    @UpdateTimestamp
-    private LocalDateTime updatedTime;
+    private Role role;
+    
+    // BaseTimeEntity에 공통화 시킴
+//    // 컬럼명에 캐멀 케이스 사용 시, db에는 created_time으로 컬럼 생성
+//    @CreationTimestamp
+//    private LocalDateTime createdTime;
+//    @UpdateTimestamp
+//    private LocalDateTime updatedTime;
 //    private String test;
 //    private String test2;
 
-    public Author(String name, String email, String password) {
+    public Author(String name, String email, String password, Role role) {
 //        this.id = AuthorMemoryRepository.id;          // db 연결 시 필요X
         this.name = name;
         this.email = email;
         this.password = password;
+        this.role = role;
     }
 
     public void updatePw(String password) {
         this.password = password;
-    }
-
-    public AuthorDetailDTO detailFromEntity() {
-        return new AuthorDetailDTO(this.id, this.name, this.email);
     }
 
     public AuthorListDTO listFromEntity() {
