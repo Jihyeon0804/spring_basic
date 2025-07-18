4319778cee81a405fe8b5bb5ad92a155be06df63
Jihyeon0804 <cam080421@naver.com>
2025-07-18 09:10:01 +0900

    Post List DTO

---
+++ src/main/java/com/beyond/basic/b2_board/post/dto/PostListDTO.java
@@ -0,0 +1,26 @@
+package com.beyond.basic.b2_board.post.dto;
+
+
+import com.beyond.basic.b2_board.post.domain.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class PostListDTO {
+
+    private Long id;
+    private String title;
+
+    public static PostListDTO fromEntity(Post post) {
+        return PostListDTO.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .build();
+    }
+
+}
