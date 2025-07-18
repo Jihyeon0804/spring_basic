ef04f02b1b3cfbcab21aa548d5a8725580a662d7
Jihyeon0804 <cam080421@naver.com>
2025-07-18 09:09:41 +0900

    Post Create DTO

---
+++ src/main/java/com/beyond/basic/b2_board/post/dto/PostCreateDTO.java
@@ -0,0 +1,33 @@
+package com.beyond.basic.b2_board.post.dto;
+
+import com.beyond.basic.b2_board.author.domain.Author;
+import com.beyond.basic.b2_board.post.domain.Post;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class PostCreateDTO {
+
+    @NotEmpty
+    private String title;
+
+    private String contents;
+
+    @NotNull                    // 숫자는 @NotEmpty 사용 불가
+    private Long authorId;
+
+    public Post toEntity(Author author) {
+        return Post.builder()
+                .title(this.title)
+                .contents(this.contents)
+                .author(author)
+                .build();
+    }
+}
