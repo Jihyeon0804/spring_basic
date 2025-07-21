73e575135a2931917f95efd755fccbf34c840baf
Jihyeon0804 <cam080421@naver.com>
2025-07-21 19:17:04 +0900

    Common DTO

--- src/main/java/com/beyond/basic/b2_board/author/dto/CommonDTO.java
+++ src/main/java/com/beyond/basic/b2_board/author/dto/CommonDTO.java
@@ -1,12 +1,14 @@
 package com.beyond.basic.b2_board.author.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class CommonDTO {
 
     private Object result;
