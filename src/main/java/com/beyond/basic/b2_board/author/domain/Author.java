09bcb85b5695a073f58234abd6127748a265a3d7
Jihyeon0804 <cam080421@naver.com>
2025-07-21 19:16:21 +0900

    Author

--- src/main/java/com/beyond/basic/b2_board/author/domain/Author.java
+++ src/main/java/com/beyond/basic/b2_board/author/domain/Author.java
@@ -12,6 +12,7 @@ import java.util.List;
 // JPA를 사용할 경우 @Entity를 반드시 붙어야하는 어노테이션 (매핑할 기준이 되는 Entity 설정을 위해 사용)
 // JPA의 EntityManager 에게 객체를 위임하기 위한 어노테이션
 // EntityManager는 영속성 컨텍스트(엔티티의 현재 상황)를 통해 DB 데이터 관리
+// @Builder를 통해 유연하게 객체 생성 가능 (@AllArgsConstructor 반드시 있어야 함)
 @Builder
 @Entity
 @Getter
@@ -38,9 +39,15 @@ public class Author extends BaseTimeEntity {
     // @OneToMany는 선택 사항, @ManyToOne과 달리 fetch 옵션의 default가 FetchType.LAZY
     // mappedBy 에는 ManyToOne 쪽에 변수명을 문자열로 지정
     // mappedBy를 지정해야 하는 이유는 FK 관리를 매핑되어 있는 (Post) 쪽에서 한다는 의미 => 연관 관계의 주인 설정
-    @OneToMany(mappedBy = "author", fetch = FetchType.LAZY)
+    // cascade : 부모 객체의 변화에 따라 자식 객체가 같이 변하는 옵션 1) CascadeType.PERSIST : 저장 2) CascadeType.REMOVE : 삭제(물리)
+    // orphanRemoval = true : 자식의 자식까지 모두 삭제하는 경우
+    @OneToMany(mappedBy = "author", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
-    List<Post> postList = new ArrayList<>();            // @OneToMany 설정 시 List 초기화 필수, @Builder.Default 설정 필수
+    List<Post> postList = new ArrayList<>();             // @OneToMany 설정 시 List 초기화 필수, @Builder.Default 설정 필수
+
+    // Address 엔티티 객체에 있는 Author의 필드명 작성
+    @OneToOne(mappedBy = "author", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private Address address;
     
     // BaseTimeEntity에 공통화 시킴
 //    // 컬럼명에 캐멀 케이스 사용 시, db 에는 created_time 으로 컬럼 생성
