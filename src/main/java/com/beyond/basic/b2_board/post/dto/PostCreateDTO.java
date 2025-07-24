1adda3e11de8ea64536eabc347ec5b78edd6abd7
Jihyeon0804 <cam080421@naver.com>
2025-07-24 09:18:22 +0900

    Post Create DTO

--- src/main/java/com/beyond/basic/b2_board/post/dto/PostCreateDTO.java
+++ src/main/java/com/beyond/basic/b2_board/post/dto/PostCreateDTO.java
@@ -8,6 +8,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -19,17 +21,25 @@ public class PostCreateDTO {
 
     private String contents;
 
+    @Builder.Default
+    private String appointment = "N";
+
+    // 시간 정보는 직접 LocalDateTime 으로 형변환하는 경우가 많음
+    private String appointmentTime;         // LocalDateTime 으로 받는 것보다는 String 으로 받는 것이 편함(사용자에게 받을 때도 String임)
+
     /*
     @NotNull                    // 숫자는 @NotEmpty 사용 불가
     private Long authorId;
      */
 
 
-    public Post toEntity(Author author) {
+    public Post toEntity(Author author, LocalDateTime appointmentTime) {
         return Post.builder()
                 .title(this.title)
                 .contents(this.contents)
                 .author(author)
+                .appointment(this.appointment)
+                .appointmentTime(appointmentTime)
                 .delYn("N")
                 .build();
     }
